83b320719405ba965c9fc743d945bd02cbeada34
Erich Kauffman <kauffm2@stolaf.edu>
2018-07-24 18:55:49 -0500

    Re-add models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    title = models.TextField()
+    author = models.TextField()
+    isbn = models.TextField()
+    edition = models.SmallIntegerField()
+    printing = models.SmallIntegerField()
+    cover = models.SmallIntegerField()
+    year_printed = models.SmallIntegerField()
+    description = models.TextField()
+    condition = models.SmallIntegerField()
+    date_purchased = models.DateField()
+    location_purchased = models.TextField()
+    amount_paid = models.IntegerField()
+    sell_price = models.IntegerField()
+    site_listed = models.SmallIntegerField()
+    shelf_location = models.TextField()
+    removal_action = models.NullBooleanField()
+    date_removed = models.DateTimeField()
