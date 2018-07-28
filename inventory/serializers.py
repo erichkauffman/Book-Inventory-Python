82e32816fb38690cbe0f5635f3bc0d9d6d956b87
Erich Kauffman <kauffm2@stolaf.edu>
2018-07-28 16:25:16 -0500

    Serialize all fields

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -4,21 +4,4 @@ from inventory.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ('title',
-                  'author', 
-                  'isbn',
-                  'edition',
-                  'printing',
-                  'cover',
-                  'year_printed',
-                  'description',
-                  'condition',
-                  'date_purchased',
-                  'location_purchased',
-                  'amount_paid',
-                  'sell_price',
-                  'site_listed',
-                  'shelf_location',
-                  'removal_action',
-                  'date_removed')
-        #fields = '__all__'
+        fields = '__all__'
