fefb09b6fda3f47b5bc1e52d7d5f06d2b1061adb
Erich Kauffman <kauffm2@stolaf.edu>
2018-07-31 20:54:07 -0500

    Add function for creating books

--- services/bookService.py
+++ services/bookService.py
@@ -1,5 +1,12 @@
 from repositories.bookRepo import BookRepo
+from data.Book import Book
 
 class BookService:
     def __init__(self, bookRepository: BookRepo):
         self.bookRepo = bookRepository
+
+    def getListOfBooks(self):
+        return self.bookRepo.getListOfBooks()
+
+    def createBook(self, book: Book):
+        self.bookRepo.createNewBook(book)
