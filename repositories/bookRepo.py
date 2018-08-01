974c59893da1b85a08ed7b8f98930e817e7ed2ef
Erich Kauffman <kauffm2@stolaf.edu>
2018-07-31 20:49:45 -0500

    Read and create book in db

--- repositories/bookRepo.py
+++ repositories/bookRepo.py
@@ -1,6 +1,37 @@
 import sqlite3
 from sqlite3 import Error
+import data.Book
+
+# Example for proper query creation:
+# db.execute('SELECT * FROM book WHERE Id = ?', 3)
 
 class BookRepo:
     def __init__(self, dbConnection: str):
-        self.db = sqlite3.connect(dbConnection).cursor()
+        try:
+            self.db = sqlite3.connect(dbConnection).cursor()
+        except Error as e:
+            print(e)
+
+    def getListOfBooks(self):
+        self.db.execute("SELECT * FROM book")
+        return self.db.fetchall()
+
+    def createNewBook(self, book: Book):
+        bookInsert = (
+            book.title,
+            book.author,
+            book.isbn,
+            book.edition,
+            book.printing,
+            book.cover,
+            book.yearPrinted,
+            book.description,
+            book.condition,
+            book.datePurchased,
+            book.locationPurchased,
+            book.amountPaid,
+            book.sellPrice,
+            book.siteListed
+        )
+        self.db.execute('INSERT INTO book VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', bookInsert)
+        self.db.commit()
