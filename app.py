195019bfbb9673ba79d3415f7b581d6bc9e0d9eb
Erich Kauffman <kauffm2@stolaf.edu>
2018-08-01 18:44:34 -0500

    Rename inventory to app

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+from flask_api import status
+import json
+
+from data.Book import Book
+from services.bookService import BookService
+from repositories.bookRepo import BookRepo
+from config import database
+
+app = Flask(__name__)
+#TODO: Move db location to config file
+bookService = BookService(BookRepo(database))
+
+@app.route('/books', methods=['GET', 'POST'])
+def books():
+    if request.method == 'GET':
+        bookList = bookService.getListOfBooks()
+        jsonBookList = json.dumps(vars(bookList), sort_keys=True, indent=4)
+        return jsonBookList
+    elif request.method == 'POST':
+        postedBook = Book(
+            request.form['title'],
+            request.form['author'],
+            request.form['isbn'],
+            request.form['edition'],
+            request.form['printing'],
+            request.form['cover'],
+            request.form['yearPrinted'],
+            request.form['description'],
+            request.form['condition'],
+            request.form['datePurchased'],
+            request.form['locationPurchased'],
+            request.form['amountPaid'],
+            request.form['sellPrice'],
+            request.form['siteListed'],
+            request.form['removalAction'],
+            request.form['dateRemoved']
+        )
+        bookService.createBook(postedBook)
+    else:
+        return status.HTTP_400_BAD_REQUEST
