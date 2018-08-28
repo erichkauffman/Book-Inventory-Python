543e017917298daa1d8136d3bcd3628d446f95e1
Erich Kauffman <kauffm2@stolaf.edu>
2018-08-27 20:07:56 -0500

    Create generic item type

---
+++ src/data/Item.py
@@ -0,0 +1,28 @@
+class Item:
+    def __init__(self,
+                 id: int,
+                 title: str,
+                 upc: str,
+                 year: int,
+                 description: str,
+                 condition: int,
+                 datePurchased: str,
+                 locationPurchased: str,
+                 amountPaid: int,
+                 sellPrice: int,
+                 siteListed: int,
+                 removalAction: bool,
+                 dateRemoved: str):
+        self.id = id
+        self.title = title
+        self.upc = upc
+        self.year = year
+        self.description = description
+        self.condition = condition
+        self.datePurchased = datePurchased
+        self.locationPurchased = locationPurchased
+        self.amountPaid = amountPaid
+        self.sellPrice = sellPrice
+        self.siteListed = siteListed
+        self.removalAction = removalAction
+        slef.dateRemoved = dateRemoved
