d2f3d4e798387d1f92352d10365bb820dda00007
Erich Kauffman <kauffm2@stolaf.edu>
2018-08-28 21:10:35 -0500

    Remove columns used by item, use foreign key to associate book and item

--- src/sqlscripts/create_book_table.sql
+++ src/sqlscripts/create_book_table.sql
@@ -1,20 +1,10 @@
 CREATE TABLE IF NOT EXISTS "book"(
-title TEXT NOT NULL,
 author TEXT NOT NULL,
-isbn TEXT NOT NULL,
 edition INT NOT NULL,
 printing INT,
 cover INT NOT NULL,
-yearPrinted INT,
-description TEXT NOT NULL,
-condition INT NOT NULL,
-datePurchased DATE NOT NULL,
-locationPurchased TEXT NOT NULL,
-amountPaid INT,
-sellPrice INT,
-siteListed INT,
-removalAction BOOLEAN,
-dateRemoved DATE);
+FOREIGN KEY (item) REFERENCES item(itemId));
+
 
 
 
