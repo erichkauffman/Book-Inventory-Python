9fa74ea52e76422f12edd8bb812fe6fb0b76db67
Erich Kauffman <kauffm2@stolaf.edu>
2018-08-28 20:44:27 -0500

    Use composition instead of inheritance

--- src/data/Book.py
+++ src/data/Book.py
@@ -1,37 +1,14 @@
 from data.Item import Item
 
-class Book(Item):
+class Book:
     def __init__(self,
-                 id: int,
-                 title: str,
+                 item: Item,
                  author: str,
-                 isbn: str,
                  edition: int,
                  printing: int,
                  cover: int,
-                 yearPrinted: int,
-                 description: str,
-                 condition: int,
-                 datePurchased: str,
-                 locationPurchased: str,
-                 amountPaid: int,
-                 sellPrice: int,
-                 siteListed: int,
-                 removalAction: bool,
                  dateRemoved: str):
-        super().__init__(id,
-                         title, 
-                         isbn,
-                         yearPrinted,
-                         description,
-                         condition,
-                         datePurchased,
-                         locationPurchased,
-                         amountPaid,
-                         sellPrice,
-                         siteListed,
-                         removalAction,
-                         dateRemoved)
+        self.item = item
         self.author = author
         self.edition = edition
         self.printing = printing
