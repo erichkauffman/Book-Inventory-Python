12ea7f4280deca3b5b107e82e9376bdaf54720d1
Erich Kauffman <kauffm2@stolaf.edu>
2018-08-30 21:52:49 -0500

    Register item routes

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes.books import bookRoutes
+from routes.items import itemRoutes
 
 app = Flask(__name__)
 
@@ -8,6 +9,7 @@ def index():
     return("Hello!")
 
 app.register_blueprint(bookRoutes, url_prefix='/books')
+app.register_blueprint(itemRoutes, url_prefix='/items')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
