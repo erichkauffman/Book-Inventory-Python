e02384df830da60d7a6373cdbf6cae44c6563b9d
Erich Kauffman <kauffm2@stolaf.edu>
2018-09-01 20:45:20 -0500

    Use assembler, json serializer

--- src/routes/books.py
+++ src/routes/books.py
@@ -1,55 +1,39 @@
-from flask import Blueprint, request, jsonify
-import json
+from flask import Blueprint, request
+import jsonpickle
 
 from data.Book import Book
 from services.bookService import BookService
+from services.itemService import ItemService
 from repositories.bookRepository import BookRepository
+from repositories.itemRepository import ItemRepository
 from lib.serializer import serialize
+from lib.convert import bookAssembler
 from config import database
 
-bookService = BookService(BookRepository(database))
+itemService = ItemService(ItemRepository(database))
+bookService = BookService(BookRepository(database), itemService)
 
 bookRoutes = Blueprint("books", __name__)
 
 @bookRoutes.route('/', methods=['GET', 'POST'])
 def books():
-    if request.method == 'GET':
-        bookList = bookService.getListOfBooks()
-        return jsonify([book.__dict__ for book in bookList])
-    elif request.method == 'POST':
-        try:
-            jsonreq = request.get_json(force=True)
-        
-            postedBook = Book(
-                0,
-                jsonreq['title'],
-                jsonreq['author'],
-                jsonreq['isbn'],
-                jsonreq['edition'],
-                jsonreq['printing'],
-                jsonreq['cover'],
-                jsonreq['yearPrinted'],
-                jsonreq['description'],
-                jsonreq['condition'],
-                jsonreq['datePurchased'],
-                jsonreq['locationPurchased'],
-                jsonreq['amountPaid'],
-                jsonreq['sellPrice'],
-                jsonreq['siteListed'],
-                jsonreq['removalAction'],
-                jsonreq['dateRemoved']
-            )
-            bookService.createBook(postedBook)
-            res = jsonify({"success": "true"})
-            return res
-        
-        except:
-            res = jsonify({"success": "false", "reason": "Something went wrong"})
-            res.status_code = 500
-            return res
+	if request.method == 'GET':
+		bookList = bookService.getListOfBooks()
+		return jsonpickle.encode(bookList)
+	elif request.method == 'POST':
+		jsonreq = request.get_json(force=True)
+		postedBook = bookAssembler(jsonreq)
+		bookService.createBook(postedBook)
+		res = jsonpickle.encode({"success": "true"})
+		return res
 
-    else:
-        res = jsonify({"success": "false", "reason":"That request method is not implemented"})
-        res.status_code = 403
-        return res
+#        except:
+#            res = jsonify({"success": "false", "reason": "Something went wrong"})
+#            res.status_code = 500
+#            return res
+
+	else:
+		res = jsonpickle.encode({"success": "false", "reason":"That request method is not implemented"})
+		res.status_code = 403
+		return res
  
