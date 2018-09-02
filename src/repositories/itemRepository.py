d29d233f336e90984493f1c57a0f4b1d90dee99e
Erich Kauffman <kauffm2@stolaf.edu>
2018-09-01 20:40:31 -0500

    Create generic item

--- src/repositories/itemRepository.py
+++ src/repositories/itemRepository.py
@@ -1,6 +1,8 @@
 import sqlite3
 import sys
 
+from data.Item import Item
+
 from lib.database import item_factory
 
 class ItemRepository:
@@ -13,3 +15,29 @@ class ItemRepository:
 		cursor = conn.cursor()
 		cursor.execute('SELECT * FROM item ORDER BY title')
 		return cursor.fetchall()
+	
+	def createNewItem(self, item: Item):
+		itemInsert = (
+			item.title,
+			item.upc,
+			item.year,
+			item.description,
+			item.condition,
+			item.datePurchased,
+			item.locationPurchased,
+			item.amountPaid,
+			item.sellPrice,
+			item.siteListed,
+			item.removalAction,
+			item.dateRemoved
+		)
+		conn = sqlite3.connect(self.dbConnection)
+		cursor = conn.cursor()
+		cursor.execute('''INSERT INTO
+							item (title, upc, year, description, condition, datePurchased,
+								  locationPurchased, amountPaid, sellPrice, siteListed, 
+								  removalAction, dateRemoved)
+							VALUES (?,?,?,?,?,?,?,?,?,?,?,?)''',
+					itemInsert)
+		conn.commit()
+		return cursor.lastrowid
