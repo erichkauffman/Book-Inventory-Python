c2743f56d444f8f5f3da12850e8fd8692d02dd6f
Erich Kauffman <kauffm2@stolaf.edu>
2018-09-01 20:38:56 -0500

    Create row factories

--- src/lib/database.py
+++ src/lib/database.py
@@ -1,23 +1,7 @@
 import sqlite3
 from data.Item import Item
 from data.Book import Book
-
-def itemAssembler(dictionary):
-	return Item(
-		dictionary['itemId'],
-		dictionary['title'],
-		dictionary['upc'],
-		dictionary['year'],
-		dictionary['description'],
-		dictionary['condition'],
-		dictionary['datePurchased'],
-		dictionary['locationPurchased'],
-		dictionary['amountPaid'],
-		dictionary['sellPrice'],
-		dictionary['siteListed'],
-		dictionary['removalAction'],
-		dictionary['dateRemoved']
-	)
+from lib.convert import itemAssembler, bookAssembler
 
 def item_factory(cursor, row):
 	dictionary = {}
@@ -29,11 +13,4 @@ def book_factory(cursor, row):
 	dictionary = {}
 	for index, columnName in enumerate(cursor.description):
 		dictionary[columnName[0]] = row[index]
-	item = itemAssembler(dictionary)
-	return Book(
-		item,
-		dictionary['author'],
-		dictionary['edition'],
-		dictionary['printing'],
-		dictionary['cover']
-	)
+	return bookAssembler(dictionary)
