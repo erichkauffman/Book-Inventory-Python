ee58742f5fb3ce6f3d532304fb4fed2434195343
Erich Kauffman <kauffm2@stolaf.edu>
2018-09-01 20:47:12 -0500

    Use item to read/create book

--- src/services/bookService.py
+++ src/services/bookService.py
@@ -1,12 +1,16 @@
 from repositories.bookRepository import BookRepository
+from services.itemService import ItemService
 from data.Book import Book
 
 class BookService:
-    def __init__(self, bookRepository: BookRepository):
-        self.bookRepo = bookRepository
+	def __init__(self, bookRepository: BookRepository, itemService: ItemService):
+		self.bookRepo = bookRepository
+		self.itemService = itemService
 
-    def getListOfBooks(self):
-        return self.bookRepo.getListOfBooks()
+	def getListOfBooks(self):
+		return self.bookRepo.getListOfBooks()
 
-    def createBook(self, book: Book):
-        self.bookRepo.createNewBook(book)
+	def createBook(self, book: Book):
+		itemId = self.itemService.createItem(book.item)
+		book.item.itemId = itemId
+		self.bookRepo.createNewBook(book)
