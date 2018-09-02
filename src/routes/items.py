d85a6666017c07d53550edfa4e686145fa6599aa
Erich Kauffman <kauffm2@stolaf.edu>
2018-09-01 20:41:41 -0500

    Add post route to item

--- src/routes/items.py
+++ src/routes/items.py
@@ -1,18 +1,24 @@
-from flask import Blueprint, request, jsonify
-import json
+from flask import Blueprint, request
+import jsonpickle
 
 from data.Item import Item
 from services.itemService import ItemService
 from repositories.itemRepository import ItemRepository
-from lib.serializer import serialize
+from lib.convert import itemAssembler
 from config import database
 
 ItemService = ItemService(ItemRepository(database))
 
 itemRoutes = Blueprint("items", __name__)
 
-@itemRoutes.route('/', methods=['GET'])
+@itemRoutes.route('/', methods=['GET', 'POST'])
 def items():
 	if request.method == 'GET':
 		itemList = ItemService.getListOfItems()
-		return jsonify([item.__dict__ for item in itemList])
+		return jsonpickle.encode(itemList)
+	elif request.method == 'POST':
+		jsonreq = request.get_json(force=True)
+		postedItem = itemAssembler(jsonreq)
+		ItemService.createItem(postedItem)
+		res = jsonpickle.encode({"success": "true"})
+		return res
