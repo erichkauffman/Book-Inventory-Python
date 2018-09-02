63a53a443f5ebffbe84a8452a8f1d5808fde24dc
Erich Kauffman <kauffm2@stolaf.edu>
2018-09-01 20:59:28 -0500

    Remove unnecessary else

--- src/lib/serializer.py
+++ src/lib/serializer.py
@@ -1,7 +1,6 @@
 import json
 
 def serialize(obj):
-    if isinstance(obj, list):
-        return json.dumps([o.__dict__ for o in obj])
-    else:
-        return json.dumps(obj.__dict__)
+	if isinstance(obj, list):
+		return json.dumps([o.__dict__ for o in obj])
+	return json.dumps(obj.__dict__)
