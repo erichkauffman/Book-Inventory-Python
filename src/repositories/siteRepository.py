c7c90654492203979e147074398215a0d350f28c
Erich Kauffman <kauffm2@stolaf.edu>
2019-02-20 17:57:51 -0600

    Insert new site into table

--- src/repositories/siteRepository.py
+++ src/repositories/siteRepository.py
@@ -3,3 +3,9 @@ import sqlite3
 class SiteRepository:
 	def __init__(self, dbConnection: str):
 		self.dbConnection = dbConnection
+
+	def setSite(self, itemId: int, site: int):
+		conn = sqlite3.connect(self.dbConnection)
+		cursor = conn.cursor()
+		cursor.execute('INSERT INTO siteListed(itemId, site) VALUES (?,?)', (itemId, site))
+		conn.commit()
