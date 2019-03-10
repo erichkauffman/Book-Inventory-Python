ad5f5cf1413af68058476e2778bc27d55f716e62
Erich Kauffman <kauffm2@stolaf.edu>
2019-03-10 16:28:11 -0500

    Remove getting all items info from database

--- src/repositories/itemRepository.py
+++ src/repositories/itemRepository.py
@@ -9,13 +9,6 @@ class ItemRepository:
 	def __init__(self, dbConnection: str):
 		self.dbConnection = dbConnection
 
-	def getListOfItems(self):
-		conn = sqlite3.connect(self.dbConnection)
-		conn.row_factory = item_factory
-		cursor = conn.cursor()
-		cursor.execute('SELECT * FROM item ORDER BY title')
-		return cursor.fetchall()
-
 	def createNewItem(self, item: Item):
 		itemInsert = (
 			item.title,
