8b06a1034aad1c8c9623cfa07a4785eb455d0b75
Erich Kauffman <kauffm2@stolaf.edu>
2019-03-10 16:23:02 -0500

    Remove unnecessary service handling for all books

--- src/services/bookService.py
+++ src/services/bookService.py
@@ -9,17 +9,11 @@ class BookService:
 		self.siteRepo = siteRepository
 		self.itemService = itemService
 
-	def getListOfBooks(self):
-		books = self.bookRepo.getListOfBooks()
-		for index, book in enumerate(books):
-			books[index].item.siteListed = self.siteRepo.getSitesById(book.item.itemId)
-		return books
-
 	def createBook(self, book: Book):
 		itemId = self.itemService.createItem(book.item)
 		book.item.itemId = itemId
 		self.bookRepo.createNewBook(book)
-	
+
 	def editBook(self, book: Book):
 		self.itemService.editItem(book.item)
 		self.bookRepo.editBook(book)
