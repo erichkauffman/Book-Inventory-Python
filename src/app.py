0b768d4a66bb12a73a9a13046e8cffae5680313b
Erich Kauffman <kauffm2@stolaf.edu>
2019-03-12 13:56:03 -0500

    Split out flask and socketio initialization from app so that socketio can be used in blueprints

--- src/app.py
+++ src/app.py
@@ -1,25 +1,11 @@
-from flask import Flask
-from flask_socketio import SocketIO
-import flask_cors
-
-from routes.books import bookRoutes
-from routes.items import itemRoutes
-from routes.locations import locationRoutes
-from routes.phrases import phraseRoutes
+from initialize import create_app, socketio
 from lib.response import makeJsonResponse
 
-app = Flask(__name__)
-socketio = SocketIO(app)
-flask_cors.CORS(app=app)
+app = create_app()
 
 @app.route('/')
 def index():
     return "Hello!"
-
-app.register_blueprint(bookRoutes, url_prefix='/books')
-app.register_blueprint(itemRoutes, url_prefix='/items')
-app.register_blueprint(locationRoutes, url_prefix='/locations')
-app.register_blueprint(phraseRoutes, url_prefix='/phrases')
 
 @app.errorhandler(404)
 def notFound(e):
