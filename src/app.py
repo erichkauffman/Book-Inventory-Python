a0fc6390f4beb80d0806d8d21cbe8713ee944e8a
Erich Kauffman <kauffm2@stolaf.edu>
2019-03-13 21:40:19 -0500

    Move in blueprint registration

--- src/app.py
+++ src/app.py
@@ -1,7 +1,18 @@
 from initialize import create_app, socketio
 from lib.response import makeJsonResponse
+from routes.books import bookRoutes
+from routes.items import itemRoutes
+from routes.locations import locationRoutes
+from routes.phrases import phraseRoutes
+import flask_cors
 
 app = create_app()
+flask_cors.CORS(app=app)
+
+app.register_blueprint(bookRoutes, url_prefix='/books')
+app.register_blueprint(itemRoutes, url_prefix='/items')
+app.register_blueprint(locationRoutes, url_prefix='/locations')
+app.register_blueprint(phraseRoutes, url_prefix='/phrases')
 
 @app.route('/')
 def index():
