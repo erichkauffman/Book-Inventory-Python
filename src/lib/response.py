fe617aabf5ccb39a5fba89f79ab2b2bbd09532a9
Erich Kauffman <kauffm2@stolaf.edu>
2019-04-10 20:35:07 -0500

    Create csv from string

--- src/lib/response.py
+++ src/lib/response.py
@@ -9,3 +9,9 @@ def makeJsonResponse(data):
 	res = make_response(jsonData)
 	res.headers['Content-Type'] = 'application/json'
 	return res
+
+def makeCsvResponse(csv):
+	res = make_response(csv)
+	res.headers['Content-Disposition'] = 'attachment; filename=db.csv'
+	res.headers['Content-type'] = 'text/csv'
+	return res
