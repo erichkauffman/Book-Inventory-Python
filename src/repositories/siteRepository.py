f4d52cb355edf07a55a06da299536984dd04ba16
Erich Kauffman <kauffm2@stolaf.edu>
2019-04-11 14:35:53 -0500

    Use site object in site repository

--- src/repositories/siteRepository.py
+++ src/repositories/siteRepository.py
@@ -1,13 +1,15 @@
 import sqlite3
 
+from data.Site import Site
+
 class SiteRepository:
 	def __init__(self, dbConnection: str):
 		self.dbConnection = dbConnection
 
-	def setSite(self, itemId: int, site: int):
+	def setSite(self, itemId: int, site: Site):
 		conn = sqlite3.connect(self.dbConnection)
 		cursor = conn.cursor()
-		cursor.execute('INSERT INTO site(itemId, site, siteId) VALUES (?,?,?)', (itemId, site))
+		cursor.execute('INSERT INTO site(itemId, site, siteId) VALUES (?,?,?)', (itemId, site.site, site.siteId))
 		conn.commit()
 
 	def getSitesById(self, itemId: int):
@@ -21,7 +23,7 @@ class SiteRepository:
 			sites.append(siteTuple[0])
 		return sites
 
-	def editSite(self, itemId: int, sites: int):
+	def editSite(self, itemId: int, sites: Site):
 		conn = sqlite3.connect(self.dbConnection)
 		cursor = conn.cursor()
 		cursor.execute('DELETE FROM site WHERE itemId = ?', (itemId,))
