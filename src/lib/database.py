4b20c84dcc7a4d2065788183a47ab127b294557c
Erich Kauffman <kauffm2@stolaf.edu>
2019-04-13 09:47:40 -0500

    Create generic factory

--- src/lib/database.py
+++ src/lib/database.py
@@ -1,21 +1,21 @@
 from lib.convert import itemAssembler, bookAssembler, siteAssembler
 
-def item_factory(cursor, row):
+def generic_factory(cursor, row):
 	dictionary = {}
 	for index, columnName in enumerate(cursor.description):
 		dictionary[columnName[0]] = row[index]
+	return dictionary
+
+def item_factory(cursor, row):
+	dictionary = generic_factory(cursor, row)
 	return itemAssembler(dictionary)
 
 def book_factory(cursor, row):
-	dictionary = {}
-	for index, columnName in enumerate(cursor.description):
-		dictionary[columnName[0]] = row[index]
+	dictionary = generic_factory(cursor, row)
 	return bookAssembler(dictionary)
 
 def site_factory(cursor, row):
-	dictionary = {}
-	for index, columnName in enumerate(cursor.description):
-		dictionary[columnName[0]] = row[index]
+	dictionary = generic_factory(cursor, row)
 	return siteAssembler(dictionary)
 
 def mini_factory(cursor, row):
