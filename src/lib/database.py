9a01698ac96e6eac286020f1e62c65aede1adbfa
Erich Kauffman <kauffm2@stolaf.edu>
2019-04-13 15:56:46 -0500

    Change item factory to handle site data

--- src/lib/database.py
+++ src/lib/database.py
@@ -7,7 +7,19 @@ def generic_factory(cursor, row):
 	return dictionary
 
 def item_factory(cursor, row):
-	dictionary = generic_factory(cursor, row)
+	dictionary = {}
+	sites = []
+	siteIds = []
+	for index, columnName in enumerate(cursor.description):
+		if columnName[0] == 'group_concat(site.site)':
+			sites = row[index].split(',')
+		elif columnName[0] == 'group_concat(site.siteId)':
+			siteIds = row[index].split(',')
+		else:
+			dictionary[columnName[0]] = row[index]
+	dictionary['siteListed'] = []
+	for site, siteId in zip(sites, siteIds):
+		dictionary['siteListed'].append({'site':int(site), 'siteId':siteId})
 	return itemAssembler(dictionary)
 
 def book_factory(cursor, row):
