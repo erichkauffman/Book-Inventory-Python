d37a60fb70cb701433237ccebd7b560321eaafba
Erich Kauffman <kauffm2@stolaf.edu>
2019-07-08 16:59:48 -0500

    Create message, pass environment

--- src/services/issueService.py
+++ src/services/issueService.py
@@ -1,8 +1,14 @@
 from repositories.issueRepository import IssueRepository
+from lib.messageHandlers import postToTrello
 
 class IssueService:
-	def __init__(self, issueRepository: IssueRepository):
+	def __init__(self, environment: str, issueRepository: IssueRepository):
+		self.environment = environment
 		self.issueRepo = issueRepository
+		self.issueTypes = ['FEATURE', 'BUG']
 
 	def createNewIssue(self, issue: dict):
 		self.issueRepo.createNewIssue(issue)
+		title = f"{self.issueTypes[issue['type']]}: {self.environment}: {issue['title']}"
+		message = {issue['message']}
+		response = postToTrello(title, message)
